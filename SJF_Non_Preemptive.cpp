b603cdb9f5e3214b9badbac663235a7590a51672
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-02-05 21:23:59 +0600

    SJF non preemptive added

---
+++ SJF_Non_Preemptive.cpp
@@ -0,0 +1,24 @@
+// SJF(Shortest Job First)
+
+//     #Given : Process Name, Arrival Time, Brust Time
+
+//     #TO_Find : Completion Time, Turn Arround Time, Waiting Time, Response Time
+
+//     #Terms : Run minimun brust time's process first
+//              Mode:   Non-Preemptive
+//              TAT = CT - AT   (CT=Completion Time, AT=Arrival Time)
+//              WT = TAT - BT
+//              Response Time = For non preemptive same as waiting time
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    freopen("output.txt","w", stdout);
+
+    cout << "Hello SJF";
+
+
+    return 0;
+}
